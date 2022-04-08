6b0f53933e3ad1440e266eefd65c8fecbcf76b84
zinh88 <zaink9898@gmail.com>
2022-04-09 02:36:18 +0500

    setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+//middleware
+app.use(express.json());
+
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
