2355dcb89e2b2040b763dd6ddbaa595bd90bf874
zinh88 <zaink9898@gmail.com>
2022-04-09 02:37:20 +0500

    edits

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("Connected to MongoDB");
-  }
+    process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log("Connected to MongoDB");
+    }
 );
 
 //middleware
@@ -18,5 +18,5 @@ app.use(express.json());
 
 
 app.listen(8800, () => {
-  console.log("Backend server is running!");
+    console.log("Backend server is running!");
 });
