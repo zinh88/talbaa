975ae31648cc1790ef26678bcbd1f49b1ca43ce2
faaiqbilal <faaiqbilal@outlook.com>
2022-04-09 17:58:12 +0500

    signup login backend dun

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,19 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const passport = require("passport");
+const users = require("./routes/api/users");
+const bodyParser = require("body-parser");
 
 dotenv.config();
 
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+//middleware
+app.use(bodyParser.json());
 mongoose.connect(
     process.env.MONGO_URL,
     { useNewUrlParser: true, useUnifiedTopology: true },
@@ -13,9 +23,12 @@ mongoose.connect(
     }
 );
 
-//middleware
-app.use(express.json());
-
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 
 app.listen(8800, () => {
     console.log("Backend server is running!");
