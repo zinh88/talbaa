c3857da790083735348cb0fe912ceda93fde7085
Haashim Ali MIrza <46709042+haashim-mirza@users.noreply.github.com>
2022-04-09 22:03:15 +0500

    navbar component with all relevant icons and general styling changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "./../components/Navbar";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Navbar />
+      <div>Home</div>
+    </>
+  );
 }
 
 export default Home;
