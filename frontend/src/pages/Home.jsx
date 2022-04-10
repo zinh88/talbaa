15075aee540f21e7909de5fad0a6863c5f1bf4c3
Miqdad1 <miqdadhasnain@gmail.com>
2022-04-10 15:25:00 +0500

    Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,49 @@
 import React from "react";
 import Navbar from "./../components/Navbar";
+import {Box2, Box, Btn, Text, BigCircBox, BigCirc, HomePage, Title, HorBox} from "./styles/Home.styled";
+import pic1 from "../assets/Homepage2.png"
+import pic2 from "../assets/Homepage1.png"
+
 
 function Home() {
   return (
     <>
-      <Navbar />
-      <div>Home</div>
+      <Navbar></Navbar>
+      <HomePage>
+        <Title>
+          Home
+        </Title>
+        <BigCircBox>
+          <Box>
+            <BigCirc pic={pic1}>
+              {/* <Picture1 pic1={pict1}/> */}
+            </BigCirc>
+            <HorBox>
+              <Box2>
+                <a style={{"text-decoration": "none"}} href="/CreateCourse">
+                <Btn>Create Courses</ Btn>
+                </a>
+              </Box2>
+              <Text>
+                Search for resources to help you clear your concepts
+              </Text>
+            </HorBox>
+          </Box>
+          
+          <Box>
+            <BigCirc pic={pic2}>
+            </BigCirc>
+            <HorBox>
+              <Box2>
+                <Btn>Find Courses</ Btn>
+              </Box2>
+              <Text>
+                Contribute to our website by sharing lectures and notes
+              </Text> 
+            </HorBox>
+          </Box>
+        </BigCircBox>
+      </HomePage>
     </>
   );
 }
