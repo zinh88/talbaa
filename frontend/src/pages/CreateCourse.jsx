b047aaae12f9df815928e41471648d45cf7084f1
Fatima Naweed <23100042@lums.edu.pk>
2022-04-12 20:33:22 +0500

    Create New Course + Add Lectures Frontend

--- frontend/src/pages/CreateCourse.jsx
+++ frontend/src/pages/CreateCourse.jsx
@@ -1,7 +1,125 @@
-import React from "react";
+import React, {useState} from "react";
+import {} from "./../CreateCourse.css";
+import Navbar from "../components/Navbar";
+
+export function PageTitle({title}){
+  const style = {
+    margin: "auto",
+    padding: "3% 0% 0% 0%",
+  }
+
+  return(
+    <h1 class="pageTitle" style = {style}>{title}</h1>
+  )
+}
+
+export function PageSubTitle({subTitle, padding}){
+  const style ={
+    margin:"auto",
+    padding: padding,
+  }
+
+  return(
+    <h1 class="pageSubTitle" style = {style}>{subTitle}</h1>
+  )
+}
+
+function Tag({name ,style}){
+  const [open, setOpen] = useState(false);
+  const [option, setOption] = useState("Dropdown");
+  return(
+    <div>
+      <PageSubTitle subTitle = {name} padding = "2% 0% 0% 3%"/>
+      <div style = {style}>
+        <h1 class="dropdown" onClick = {() => {setOpen(!open)}}>
+          {option}
+        </h1>
+
+        {open && <DropDown setOption = {setOption} setOpen = {setOpen}/>}
+
+      </div>
+    </div>
+    
+  )
+}
+
+function DropDown({setOption, setOpen}){
+  
+  const itemStyle = {
+    padding: "0.5rem"
+  }
+
+  function DropdownItem(props){
+    return(
+      <a>
+        <h1 class="menu-item" style={itemStyle} onClick={() => {setOption(props.children)
+                                              setOpen(false)}}>
+          {props.children}
+        </h1>
+      </a>
+    )
+  }
+
+  return(
+    <div >
+      <DropdownItem>Machine Learning</DropdownItem>
+      <DropdownItem>Biology</DropdownItem>
+      <DropdownItem>Maths</DropdownItem>
+    </div>
+  )
+}
+
+function CourseInfo({name, style, placeholderType, padding}){
+  return(
+    <div>
+      <PageSubTitle subTitle = {name} padding = {padding}/>
+        <div style = {style}>
+          <input placeholder="Placeholder..." class={placeholderType}></input>
+        </div>
+    </div>
+  )
+}
+
+export function Button({text}){
+
+  const button = {
+    padding: "1% 0% 0% 2%",
+  }
+
+  return(
+    <div class="column" style={button}>
+          <a style={{"text-decoration": "none"}} href="#"> 
+            <h1 class="addButton" >{text}</h1>
+          </a>
+    </div>
+  )
+}
+
 
 function CreateCourse() {
-  return <div>CreateCourse</div>;
+
+  const textBox = {
+    padding: "1% 0% 0% 2%",
+  }
+
+  return (
+    <div>
+      <Navbar/>
+      <PageTitle title = "New Course"/>
+
+      <CourseInfo name="Course Title*" style={textBox} 
+                  placeholderType = "placeholderTitle" padding = "5% 0% 0% 3%" />
+      <CourseInfo name="Course Description*" style={textBox} 
+                  placeholderType = "placeholderDescription" padding = "3% 0% 0% 3%" />
+
+      <Tag name="Tag 1*" style={textBox}/>
+      <Tag name="Tag 2*" style={textBox}/>
+      <Tag name="Tag 3*" style={textBox}/>
+
+      <Button text="Add Lectures!"/>
+
+    </div>
+  )
 }
 
 export default CreateCourse;
