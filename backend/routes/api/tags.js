151a3bd23ca7b088c7630983955c7d1cf7e5d6af
faaiqbilal <faaiqbilal@outlook.com>
2022-04-13 21:06:40 +0500

    tags backend done

--- backend/routes/api/tags.js
+++ backend/routes/api/tags.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const dotenv = require("dotenv");
-const { Tags } = require("../../models/Tags")
+const jwt = require("jsonwebtoken");
+const Tags = require("../../models/Tags")
 dotenv.config()
 
 router.get("/get_tags", async(req, res) => {
     const recvTags = await Tags.find();
     res.send(recvTags)
+});
+
+router.post("/add_tag", (req, res) => {
+    const token = req.headers['authorization'].split(' ')[1];
+    const payload = jwt.verify(token, process.env.secretOrKey)
+    const newTag = new Tags({
+        name : req.body.name
+    });
+
+    newTag 
+        .save()
+        .then(tag => res.json(tag))
+        .catch(err => console.log(err));
 });
 
 module.exports = router;
