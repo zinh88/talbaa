40d5b7a42a4146f21d50e6313c077948567d4a36
Haashim Ali MIrza <46709042+haashim-mirza@users.noreply.github.com>
2022-04-17 20:54:24 +0500

    heroku redeploy

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import { Tag } from "../pages/CreateCourse";
 
 function Navbar() {
+  let [signout, setSignout] = useState(false);
   return (
     <div className="header">
       <a href="/">
@@ -10,7 +12,17 @@ function Navbar() {
       <a href="/home">Home</a>
       <a href="/courseCatalogue">Course Catalogue</a>
       <a href="/myCourses">My Courses</a>
-      <img src="/girlicon.png" height="60vw" alt="logo"></img>
+      <img
+        src="/girlicon.png"
+        height="60vw"
+        alt="logo"
+        style={{ cursor: "pointer" }}
+        onClick={() => {
+          setSignout(!signout);
+          console.log(signout);
+        }}
+      ></img>
+      {signout ? <a href="/">Sign Out</a> : <></>}
     </div>
   );
 }
