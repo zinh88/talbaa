211f850bc8f6494639ddc93d9233bb37add113dc
Haashim Ali MIrza <46709042+haashim-mirza@users.noreply.github.com>
2022-04-18 00:27:17 +0500

    merge

--- frontend/src/pages/EditLecture.jsx
+++ frontend/src/pages/EditLecture.jsx
@@ -1,24 +1,29 @@
 import React, { useState } from "react";
 import UploadButton from "../components/UploadButton";
 import Navbar from "./../components/Navbar";
-import { Image, Video } from "cloudinary-react";
-import {} from "./../EditLecture.css";
+import { Image, Video} from "cloudinary-react";
+import {} from './../EditLecture.css'
 import { CourseInfo } from "./CreateCourse";
 import {} from "./../CreateCourse.css";
 import { blackwhite } from "@cloudinary/url-gen/actions/effect";
 import { border } from "@cloudinary/url-gen/qualifiers/background";
 
-function InsertImage({ id, cloudName }) {
-  return (
-    <div class="image">
-      <Image cloudName={cloudName} publicId={id} width="70%" />
+
+function InsertImage({id, cloudName}){
+  return(
+    <div class = "image">
+      <Image
+        cloudName={cloudName}
+        publicId={id}
+        width="70%"
+      />
     </div>
-  );
+  )
 }
 
-function InsertPDF({ id, cloudName, name }) {
-  return (
-    <div class="pdfLinks">
+function InsertPDF({id, cloudName, name}){
+  return(
+    <div class = "pdfLinks">
       <a
         href={`http://res.cloudinary.com/${cloudName}/image/upload/${id}`}
         target="_blank"
@@ -26,18 +31,19 @@ function InsertPDF({ id, cloudName, name }) {
         {name}
       </a>
     </div>
-  );
+  )
 }
 
-function InsertVideo({ id, cloudName }) {
-  return (
+function InsertVideo({id, cloudName}){
+  return(
     <div class="video">
-      <Video width="95%" cloudName={cloudName} publicId={id} controls="true" />
+      <Video width= "95%" cloudName={cloudName} publicId={id} controls="true"/>
     </div>
-  );
+  )
 }
 
-function InsertTextBox({ resources, setResources }) {
+function InsertTextBox({resources,setResources}){
+
   const [title, setTitle] = useState("");
   const [note, setNote] = useState("");
 
@@ -46,197 +52,215 @@ function InsertTextBox({ resources, setResources }) {
   };
 
   const borderBox = {
-    border: "1px solid black",
-    "margin-top": "5%",
-    "margin-left": "5%",
-    "padding-bottom": "3%",
-    width: "900px",
-  };
-
-  function SubmitButton({ title, note, resources, setResources }) {
-    const buttonStyle = {
-      "padding-top": "0.35rem",
-      "font-size": "15px",
-      "margin-top": "3%",
-      "margin-left": "4%",
-    };
-
-    return (
-      <div
-        class="style"
-        style={buttonStyle}
-        onClick={() => {
-          let JSONobj = { id: { note }, name: { title }, type: "textBox" };
-          setResources([...resources, JSONobj]);
-        }}
-      >
-        Submit
-      </div>
-    );
+    "border" : "1px solid black",
+    "margin-top":"5%",
+    "margin-left":"5%",
+    "padding-bottom":"3%",
+    "width": "900px"
   }
 
-  return (
-    <div style={borderBox}>
-      <CourseInfo
-        name="Title*"
-        style={textBox}
-        placeholderType="placeholderTitle"
-        padding="5% 0% 0% 3%"
-        state={title}
-        setState={setTitle}
+  function SubmitButton({title, note, resources, setResources}){
+    const buttonStyle = {
+      "padding-top":"0.35rem",
+      "font-size":"15px",
+      "margin-top":"3%",
+      "margin-left":"4%"
+    }
+    
+    return(
+      <div  class = "style" style = {buttonStyle}
+            onClick = {() => {
+              let JSONobj = { id: {note},
+                              name: {title},
+                              type: "textBox"}
+              setResources([...resources,JSONobj])
+            }}>
+        Submit
+      </div>
+    )
+  }
+
+
+  return(
+    <div style = {borderBox}>
+
+      <CourseInfo  
+      name = "Title*"
+      style = {textBox}
+      placeholderType = "placeholderTitle"
+      padding = "5% 0% 0% 3%"
+      state = {title}
+      setState = {setTitle}
       />
-      <CourseInfo
-        name="Text*"
-        style={textBox}
-        placeholderType="placeholderDescription"
-        padding="5% 0% 0% 3%"
-        state={note}
-        setState={setNote}
+      <CourseInfo  
+      name = "Text*"
+      style = {textBox}
+      placeholderType = "placeholderDescription"
+      padding = "5% 0% 0% 3%"
+      state = {note}
+      setState = {setNote}
       />
 
       <SubmitButton
-        title={title}
-        note={note}
-        resources={resources}
-        setResources={setResources}
+        title = {title}
+        note = {note}
+        resources = {resources}
+        setResources = {setResources}
       />
     </div>
-  );
+  )
 }
 
-function DisplayContent({ resources, cloudName, setResources }) {
+function DisplayContent({resources, cloudName, setResources}){
+  
   const textStyle = {
-    "margin-left": "5%",
-    "margin-top": "4%",
-  };
-
-  return (
+    "margin-left":"5%",
+    "margin-top":"4%"
+  }
+  
+  return(
     <div>
-      {resources.map((value) => {
-        if (value.type == "image") {
-          return <InsertImage id={value.id} cloudName={cloudName} />;
-        } else if (value.type == "pdf") {
-          return (
-            <InsertPDF id={value.id} cloudName={cloudName} name={value.name} />
-          );
-        } else if (value.type == "textBox") {
-          return (
-            <div style={textStyle}>
-              <h1>{value.name.title}</h1>
-              <h3>{value.id.note}</h3>
-            </div>
-          );
-        } else if (value.type == "video") {
-          return <InsertVideo id={value.id} cloudName={cloudName} />;
-        }
-      })}
+      {
+        resources.map((value) => {
+          if (value.type == "image"){
+            return <InsertImage 
+                    id={value.id} 
+                    cloudName={cloudName}/>
+          } else if (value.type == "pdf"){
+            return <InsertPDF 
+                    id={value.id} 
+                    cloudName={cloudName} 
+                    name={value.name}/>
+          } else if (value.type == "textBox"){
+            return (
+              <div style = {textStyle}>
+                <h1>{value.name.title}</h1>
+                <h3>{value.id.note}</h3>
+              </div>
+            )
+          } else if (value.type == "video"){
+            return <InsertVideo 
+                    id={value.id} 
+                    cloudName={cloudName}/>
+          } 
+        })
+      }
     </div>
-  );
+  )
 }
 
-function DropDown({ resources, setResources, textForm, setTextForm }) {
+function DropDown({resources,setResources, textForm, setTextForm}) {
   const itemStyle = {
-    "font-size": "13px",
-    "padding-top": "0.5rem",
+    "font-size" : "13px",
+    "padding-top": "0.5rem"
   };
 
   const textEvent = () => {
     // console.log("Enter Event here")
-    return setTextForm(!textForm);
-  };
+    return(
+      setTextForm(!textForm)
+    )
+  }
 
   const quizEvent = () => {
-    console.log("Enter Event here");
-  };
+    console.log("Enter Event here")
+  }
+  
 
-  function AddResourceButton({ text, event }) {
-    return (
+  function AddResourceButton({text,event}){
+    return(
       <div>
-        <p class="style" style={itemStyle} onClick={event}>
+        <p  class = "style" 
+            style = {itemStyle}
+            onClick = {event}>
           {text}
         </p>
       </div>
-    );
+    )
   }
 
-  function GenerateDropdown() {
-    return (
+  function GenerateDropdown(){
+    return(
       <div>
-        <UploadButton />
-        <AddResourceButton text="Add Text Box" event={textEvent} />
-        <AddResourceButton text="Add Quiz" event={quizEvent} />
+        <UploadButton/>
+        <AddResourceButton 
+          text = "Add Text Box" event = {textEvent} />
+        <AddResourceButton 
+          text = "Add Quiz" event = {quizEvent} />
       </div>
-    );
+    )
   }
 
   return (
     <div>
-      <GenerateDropdown />
+      <GenerateDropdown/>
     </div>
   );
 }
 
-function AddButton({ event, text }) {
+function AddButton({event,text}){
   const style = {
-    width: "2rem",
-    height: "2rem",
+    "width": "2rem",
+    "height": "2rem",
     "border-radius": "1rem",
     "font-size": "2rem",
     "font-weight": "600",
     "text-decoration": "none",
-  };
+  }
 
-  return (
-    <div class="clickButton">
-      <h1 class="clickText" style={style} onClick={event}>
-        {text}
-      </h1>
+  return(
+    <div class="clickButton" >
+      <h1 class="clickText" 
+          style={style}
+          onClick = {event} >
+          {text}
+      </h1> 
     </div>
-  );
+  )
 }
 
-function ResButton({ resources, setResources, textForm, setTextForm }) {
+function ResButton({resources,setResources,textForm, setTextForm}){
+
   const textStyle = {
     "padding-top": "clamp(4%,4%,4%)",
-    "font-size": "100%",
+    "font-size" : "100%",
     "padding-left": "clamp(20px, 20px, 30px)",
     "padding-right": "clamp(20%, 20%, 50%%)",
-  };
+  }
 
   const buttonStyle = {
     "padding-left": "clamp(10%, 30%, 50%)",
     "padding-top": "clamp(3%,3%,3%)",
-  };
+  }
 
   const [open, setOpen] = useState(false);
 
-  return (
+  return(
     <div class="insertBox">
-      <div class="clickButton">
-        <div class="row">
-          <div style={buttonStyle}>
-            <AddButton
-              event={() => {
-                setOpen(!open);
-              }}
-              text="+"
-            />
+        <div class = "clickButton">
+          <div class = "row">
+            <div style = {buttonStyle}>
+              <AddButton 
+              event = {
+                () => {setOpen(!open);}
+              }
+              text = "+"
+              />
+            </div>
+            <div style = {textStyle}>
+              Add Resources
+            </div>
           </div>
-          <div style={textStyle}>Add Resources</div>
+          <div class = "shiftDropdown">
+            {open && <DropDown
+                      resources = {resources}
+                      setResources = {setResources}
+                      textForm = {textForm}
+                      setTextForm = {setTextForm}/>}
+          </div>
         </div>
-        <div class="shiftDropdown">
-          {open && (
-            <DropDown
-              resources={resources}
-              setResources={setResources}
-              textForm={textForm}
-              setTextForm={setTextForm}
-            />
-          )}
-        </div>
-      </div>
-    </div>
-  );
+    </div>  
+  )
 }
 
 // const cld = new Cloudinary({
@@ -276,6 +300,7 @@ function EditLecture() {
         />
       </Video>
     </div>
+
   );
 }
 export default EditLecture;
